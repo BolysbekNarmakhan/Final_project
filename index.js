81255b2b981c2c92f890980c6652f52310435dfd
BolysbekNarmakhan <210103223@stu.sdu.edu.kz>
2022-12-18 19:15:00 +0600

    five

--- index.js
+++ index.js
@@ -1,12 +1,74 @@
 //sign in
-const switchers = [...document.querySelectorAll('.switcher')]
+const form = document.getElementById('form');
+const username = document.getElementById('username');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+const passsword = document.getElementById('passsword');
 
-switchers.forEach(item => {
-	item.addEventListener('click', function() {
-		switchers.forEach(item => item.parentElement.classList.remove('is-active'))
-		this.parentElement.classList.add('is-active')
-	})
-})
+
+form.addEventListener('submit',(e)=>{
+    e.preventDefault();
+
+    checkInputs();
+} );
+
+function checkInputs(){
+    const usernameValue = username.value.trim();
+    const emailValue = email.value.trim();
+    const passwordValue = password.value.trim();
+    const passswordValue = passsword.value.trim();
+    if(usernameValue === ''){
+               //show error
+        setErrorFor(username,"Username can not be blank");
+    }else{
+              //add succes class
+        setSuccessFor(username);
+    }
+    if(emailValue === ''){
+        setErrorFor(email , 'Email cannot be blank');
+    } else if (!isEmail(emailValue)) {
+        setErrorFor(email, 'Email is not valid');
+    } else {
+        setSuccessFor(email);
+    }
+
+    if(passwordValue === ''){       
+        //show error
+        setErrorFor(password, "Password can not be blank");
+    } else {
+                //add succes class
+        setSuccessFor(password);
+    
+    } 
+    if(passswordValue === ''){       
+        setErrorFor(passsword,"Password can not be blank");
+    }else if(passwordValue !== passswordValue){
+        setErrorFor(passsword,"Password does not match");
+    }else{
+        setSuccessFor(passsword);
+    
+    }
+}
+
+
+
+function setErrorFor(input,message){
+    const formControl = input.parentElement;
+    const small = formControl.querySelector('small');
+
+    small.innerText = message;
+
+    formControl.className = 'form-control error';
+}
+
+function setSuccessFor(input){
+    const formControl = input.parentElement;
+    formControl.className ='form_control succes'
+}
+
+function isEmail(email){
+    return  /[0-9][_-=!@#$%^&*()][a-zA-Z]/.test(email);
+}
 
 
 //podelit
